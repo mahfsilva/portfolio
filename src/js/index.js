fdf6512732470b4f841fda6f366203df90830fa1
mahfsilva <166265205+mahfsilva@users.noreply.github.com>
2024-04-06 22:09:58 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,31 @@
+/*- Objetivo 1 - quando o usuário clicar no botão de mostrar mais deve abrir os projetos que estão escondidos no html 
+Passo 1 - pegar o botão mostrar mais no JS pra poder verificar quando o usuário clicar em cima dele 
+Passo 2 - identificar o clique no botão 
+Passo 3 - adicionar a classe ativo nos projetos escondidos 
+
+- Objetivo 2 - esconder o botão de mostrar mais 
+Passo 1 - pegar o botão e esconder ele */
+
+//Objetivo 1 - quando o usuário clicar no botão de mostrar mais deve abrir os projetos que estão escondidos no html 
+//Passo 1 - pegar o botão mostrar mais no JS pra poder verificar quando o usuário clicar em cima dele 
+
+const btnMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+
+
+
+btnMostrarProjetos.addEventListener('click', () => {
+    mostrarMaisProjetos();
+    esconderBotao();
+
+});
+
+function esconderBotao() {
+    btnMostrarProjetos.classList.add("remover");
+}
+
+function mostrarMaisProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.add('ativo');
+    });
+}
